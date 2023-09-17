e4e667ca443598cf626bf8f248e010ae1af23cc0
v.v.georgi@gmail.com <v.v.georgi@gmail.com>
2023-09-17 14:14:48 +0300

    conect to JSON

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-const jsonURL = './data.json';
-const request = new Request(jsonURL);
-const response = await fetch(request);
-const results = response.json;
-function doIt (){
-    console.log("asdasd");
-}
+function doIt() {
 
+    let myData = fetch("./data.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        return data;
+    })
+
+    myData.then(function(score){
+        const reaction = document.getElementById('reaction-score') ;
+        reaction.innerText = score[0].score;
+        const memory = document.getElementById('memory-score');
+        memory.innerText = score[1].score;
+        const verbal = document.getElementById('verbal-score'); 
+        verbal.innerText = score[2].score;
+        const visual = document.getElementById('visual-score');
+        visual.innerText = score[3].score;
+
+        const allPicElements = document.getElementsByClassName('scores-icons');
+        const allScoresElements = document.getElementsByClassName('score_variable');
+        
+        for (let i = 0; i<allPicElements.length; i++){
+            allPicElements.item(i).src=score[i].icon;
+            allScoresElements.item(i).innerText = score[i].score;
+
+        }
+        
+
+    })
+}
